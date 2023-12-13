90e3c079e4fde9e30cc4cdfea83ab385c0f73b4f
sers1 <sers156@mail.ru>
2023-12-13 16:50:42 +1000

    hw10

--- src/Exercise1.java
+++ src/Exercise1.java
@@ -1,12 +1,16 @@
 public class Exercise1 {
+
     static double umnojenie(double a, double b, double c){
         return a*b*c;
     }
+
     static void delenie(int a, int b){
         System.out.println("Целое частное =" + a/b +"  Остаток =" + a % b);
     }
 }
+
 class Exercise1Test {
+
     public static void main(String[] args) {
         System.out.println(Exercise1.umnojenie(4,5,6));
         Exercise1.delenie(9,4);
--- src/Exercise2.java
+++ src/Exercise2.java
@@ -1,24 +1,23 @@
 public class Exercise2 {
 
-public  final  static double PI = 3.14;
+    public final static double PI = 3.14;
 
-public double ploshad(double radius) {
-    double p1 = PI * radius *radius;
-    return p1;
-}
+    public double ploshad(double radius) {
+        double p1 = PI * radius * radius;
+        return p1;
+    }
 
-public static double dlina(double radius2){
-    double d1 = 2 * PI * radius2;
-    return d1;
-}
+    public static double dlina(double radius2) {
+        double d1 = 2 * PI * radius2;
+        return d1;
+    }
 
-public void info(double radius3){
-    System.out.println("Радиус =" + radius3);
-    System.out.println("Площадь круга =" + ploshad(radius3));
-    System.out.println("Радиус =" + dlina(radius3));
+    public void info(double radius3) {
+        System.out.println("Радиус =" + radius3);
+        System.out.println("Площадь круга =" + ploshad(radius3));
+        System.out.println("Радиус =" + dlina(radius3));
+    }
 }
-}
-
 class Exercise2Test {
 
     public static void main(String[] args) {
